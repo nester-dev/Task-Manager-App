6c0920d511a41d9ca944c4311fa895864f0013fc
nester-dev <nester.dev@gmail.com>
2022-12-22 18:26:51 +0300

    fix: backend2

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'final-task-backend-production-9cb1.up.railway.app';
+export const BASE_URL = 'https://final-task-backend-production-9cb1.up.railway.app';
 export const LINKS = {
   welcome: `/`,
   search: '/search',
